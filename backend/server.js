1724d704bd7479139ca19be70e7ac35f2b03e091
Priyanshi Khetwani <PriyanshiKhetwani@gmail.com>
2021-05-15 00:02:09 +0530

    products route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ import data from "./data.js";
 
 const app = express();
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
@@ -11,7 +20,8 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serving at port: ${port}`);
+  console.log(`serving at port : ${port}`);
 });
