e55d774adaeff9cf35f2ac35c061e7cb151ec99e
Priyanshi Khetwani <PriyanshiKhetwani@gmail.com>
2021-05-22 09:13:41 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 // import data from "./data.js";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
@@ -27,6 +32,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
